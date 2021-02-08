11ab7836cb47cea111fc0771e0b4a44fb6385ebc
Miguel Ricaño <mricanho@gmail.com>
2021-02-08 12:21:29 -0600

    Add gem database cleaner

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'sqlite3', '~> 1.4'
+  gem 'database_cleaner', '~> 2.0', '>= 2.0.1'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :attendances
+  resources :attendances, skip: [:destroy, :edit, :show, :update]
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :events, skip: [:destroy, :edit, :show, :update]
   root "events#index"
