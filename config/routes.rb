1cec07a3604ab2d47a2ac6eabfc30024aee06c17
Miguel Ricaño <mricanho@gmail.com>
2021-02-08 11:04:52 -0600

    Update routes in config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :attendances
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :events, skip: [:destroy, :edit, :show, :update]
   root "events#index"
 end
