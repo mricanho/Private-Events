1151a268a9290fb69e4efb1513fcadc71f2ea9e8
Miguel Ricaño <mricanho@gmail.com>
2021-02-07 19:09:59 -0600

    Fix validations error for the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,6 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :attendee, through: :attendances, source: :user
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :date, presence: true
-
   scope :upcoming, -> { where('date >= ?', DateTime.now) }
   scope :past, -> { where('date < ? ', DateTime.now) }
 end
